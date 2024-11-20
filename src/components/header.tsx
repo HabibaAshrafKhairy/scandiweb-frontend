e79583822b21e6c13c6c46840ae34234cf18f79b
habibaashrafkhairy <habibakhairy00@gmail.com>
2024-11-20 20:21:34 +0200

    cart total items

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,12 +6,13 @@ import CartOverlay from "./cartOverlay";
 
 interface State {
   showCartOverlay: boolean;
+  totalItems: number;
 }
 
 class Header extends React.Component<{}, State> {
   constructor(props: {}) {
     super(props);
-    this.state = { showCartOverlay: false };
+    this.state = { showCartOverlay: false, totalItems: 3 };
   }
 
   render(): React.ReactNode {
@@ -22,14 +23,22 @@ class Header extends React.Component<{}, State> {
         <div>
           <img src={logo} alt="logo" />
         </div>
+
         <button
           onClick={() => {
             this.setState((prev) => {
               return { showCartOverlay: !prev.showCartOverlay };
             });
           }}
+          className="relative"
         >
           <img src={cartIcon} alt="cart icon" />
+
+          {this.state.totalItems > 0 && (
+            <div className="w-5 h-5 bg-[#1D1F22] rounded-full absolute top-[10px] left-3/4 text-white font-bold flex items-center justify-center">
+              {this.state.totalItems}
+            </div>
+          )}
         </button>
 
         {this.state.showCartOverlay && <CartOverlay />}
