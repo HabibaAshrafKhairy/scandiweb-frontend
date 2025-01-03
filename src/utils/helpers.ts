2bda87a953c05b9bcca726cd0fe07530dbbfc457
HabibaAshrafKhairy <habibakhairy00@gmail.com>
2025-01-03 22:33:04 +0200

    added tokebabcase helper function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -10,3 +10,10 @@ export function removeTags(str: string) {
 
   return str.replace(/(<([^>]+)>)/gi, "");
 }
+
+export function toKebabCase(str: string): string {
+  return str
+
+    .replace(/\s+/g, "-") // Replace spaces with hyphens
+    .toLowerCase(); // Convert the string to lowercase
+}
